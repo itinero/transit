a4712e0589005dd7c066cef7f6e22cafbe010d73
xivk <ben@osmsharp.com>
2015-01-05 14:13:33 +0100

    Added a test to prevent transfers.

--- OsmSharp.Transit.Test/Routing/MultiModal/RouteCalculators/TinyTransitTests.cs
+++ OsmSharp.Transit.Test/Routing/MultiModal/RouteCalculators/TinyTransitTests.cs
@@ -1286,5 +1286,249 @@ namespace OsmSharp.Transit.Test.Routing.MultiModal.RouteCalculators
             Assert.AreEqual(coordinate5.Latitude, route.Segments[5].Latitude, E);
             Assert.AreEqual(coordinate5.Longitude, route.Segments[5].Longitude, E);
         }
+
+        /// <summary>
+        /// Tests a route in the a basic network.
+        /// </summary>
+        /// <remarks>
+        ///                                     (1)           (2)              (3)            (4)
+        ///          The network has 5 vertices: 0 --- 1km --- 1 ---- 10km ---- 2 --- 10km --- 3.
+        ///          The network has stops:                STOP1(5)          STOP2(6)       STOP3(7)
+        ///          And TRIP1 01/01/2015:                  10:00             10:10    
+        ///          And TRIP2 01/01/2015:                  10:10          10:20->10:21      10:31
+        ///          
+        ///          This means when going from 0 -> 3 TRIP2 should be taken all the way including to STOP2 even though TRIP1 leaves earlier.
+        ///          What is tested: - a trip 0 -> 2 on foot leaving 01/01/2015 09:45: should take TRIP1.
+        ///                          - a trip 0 -> 2 on foot leaving 01/01/2015 09:45: should take TRIP2.
+        /// </remarks>
+        [Test]
+        public void Test1Transit6TwoOverlappingTrips()
+        {
+            var vehicle = Vehicle.Pedestrian;
+            var coordinate0 = new GeoCoordinate(51.263902062418180, 4.778001308441162);
+            var coordinate1 = new GeoCoordinate(51.264022903457850, 4.792361855506897);
+            var coordinate2 = new GeoCoordinate(51.265137311403734, 4.936380386352539);
+            var coordinate3 = new GeoCoordinate(51.266318822846884, 5.079803466796875);
+
+            var tags = new TagsTableCollectionIndex();
+            var graph = new DynamicGraphRouterDataSource<LiveEdge>(tags);
+            var vertex0 = graph.AddVertex((float)coordinate0.Latitude, (float)coordinate0.Longitude);
+            var vertex1 = graph.AddVertex((float)coordinate1.Latitude, (float)coordinate1.Longitude);
+            var vertex2 = graph.AddVertex((float)coordinate2.Latitude, (float)coordinate2.Longitude);
+            var vertex3 = graph.AddVertex((float)coordinate3.Latitude, (float)coordinate3.Longitude);
+            graph.AddEdge(vertex0, vertex1, new LiveEdge()
+            {
+                Distance = 1000,
+                Forward = true,
+                Tags = tags.Add(new TagsCollection(new Tag("highway", "residential")))
+            });
+            graph.AddEdge(vertex1, vertex2, new LiveEdge()
+            {
+                Distance = 10000,
+                Forward = true,
+                Tags = tags.Add(new TagsCollection(new Tag("highway", "residential")))
+            });
+            graph.AddEdge(vertex2, vertex3, new LiveEdge()
+            {
+                Distance = 10000,
+                Forward = true,
+                Tags = tags.Add(new TagsCollection(new Tag("highway", "residential")))
+            });
+
+            var feed = new GTFSFeed();
+            feed.AddAgency(new GTFS.Entities.Agency()
+            {
+                Id = "0",
+                Name = "Agency"
+            });
+            feed.AddStop(new GTFS.Entities.Stop()
+            {
+                Id = "0",
+                Code = "Code 0",
+                Name = "Name 0",
+                Latitude = coordinate1.Latitude,
+                Longitude = coordinate1.Longitude
+            });
+            feed.AddStop(new GTFS.Entities.Stop()
+            {
+                Id = "1",
+                Code = "Code 1",
+                Name = "Name 1",
+                Latitude = coordinate2.Latitude,
+                Longitude = coordinate2.Longitude
+            });
+            feed.AddStop(new GTFS.Entities.Stop()
+            {
+                Id = "2",
+                Code = "Code 2",
+                Name = "Name 2",
+                Latitude = coordinate3.Latitude,
+                Longitude = coordinate3.Longitude
+            });
+            feed.AddRoute(new GTFS.Entities.Route()
+            {
+                AgencyId = "0",
+                Id = "0",
+                ShortName = "Route 0"
+            });
+            feed.AddRoute(new GTFS.Entities.Route()
+            {
+                AgencyId = "1",
+                Id = "1",
+                ShortName = "Route 1"
+            });
+            feed.AddTrip(new GTFS.Entities.Trip()
+            {
+                Id = "0",
+                RouteId = "0",
+                ServiceId = "0",
+                ShortName = "Trip 0"
+            });
+            feed.AddTrip(new GTFS.Entities.Trip()
+            {
+                Id = "1",
+                RouteId = "1",
+                ServiceId = "0",
+                ShortName = "Trip 1"
+            });
+            feed.AddCalendarDate(new GTFS.Entities.CalendarDate()
+            {
+                Date = new DateTime(2015, 01, 01),
+                ExceptionType = GTFS.Entities.Enumerations.ExceptionType.Added,
+                ServiceId = "0"
+            });
+            feed.AddStopTime(new GTFS.Entities.StopTime()
+            {
+                TripId = "0",
+                StopId = "0",
+                ArrivalTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 0,
+                    Seconds = 0
+                },
+                DepartureTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 0,
+                    Seconds = 0
+                },
+                StopSequence = 1
+            });
+            feed.AddStopTime(new GTFS.Entities.StopTime()
+            {
+                TripId = "0",
+                StopId = "1",
+                ArrivalTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 10,
+                    Seconds = 0
+                },
+                DepartureTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 10,
+                    Seconds = 0
+                },
+                StopSequence = 2
+            });
+            feed.AddStopTime(new GTFS.Entities.StopTime()
+            {
+                TripId = "1",
+                StopId = "0",
+                ArrivalTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 10,
+                    Seconds = 0
+                },
+                DepartureTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 10,
+                    Seconds = 0
+                },
+                StopSequence = 1
+            });
+            feed.AddStopTime(new GTFS.Entities.StopTime()
+            {
+                TripId = "1",
+                StopId = "1",
+                ArrivalTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 20,
+                    Seconds = 0
+                },
+                DepartureTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 21,
+                    Seconds = 0
+                },
+                StopSequence = 2
+            });
+            feed.AddStopTime(new GTFS.Entities.StopTime()
+            {
+                TripId = "1",
+                StopId = "2",
+                ArrivalTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 31,
+                    Seconds = 0
+                },
+                DepartureTime = new TimeOfDay()
+                {
+                    Hours = 10,
+                    Minutes = 31,
+                    Seconds = 0
+                },
+                StopSequence = 3
+            });
+
+
+            var router = MultiModalRouter.CreateFrom(graph);
+            router.AddGTFSFeed(feed);
+            var resolved0 = router.Resolve(vehicle, coordinate0);
+            var resolved2 = router.Resolve(vehicle, coordinate2);
+            var resolved3 = router.Resolve(vehicle, coordinate3);
+
+            // a trip 0 -> 3 on foot leaving 01/01/2015 09:45: should take the transit link.
+            var route = router.CalculateTransit(new DateTime(2015, 01, 01, 09, 45, 00), vehicle, vehicle, vehicle, resolved0, resolved2);
+            Assert.IsNotNull(route);
+            Assert.IsNotNull(route.Segments);
+            Assert.AreEqual(5, route.Segments.Length);
+            Assert.AreEqual(coordinate0.Latitude, route.Segments[0].Latitude, E);
+            Assert.AreEqual(coordinate0.Longitude, route.Segments[0].Longitude, E);
+            Assert.AreEqual(coordinate1.Latitude, route.Segments[1].Latitude, E);
+            Assert.AreEqual(coordinate1.Longitude, route.Segments[1].Longitude, E);
+            Assert.AreEqual(coordinate1.Latitude, route.Segments[2].Latitude, E);
+            Assert.AreEqual(coordinate1.Longitude, route.Segments[2].Longitude, E);
+            Assert.AreEqual(coordinate2.Latitude, route.Segments[3].Latitude, E);
+            Assert.AreEqual(coordinate2.Longitude, route.Segments[3].Longitude, E);
+            Assert.IsTrue(route.Segments[3].Tags.ConvertToTagsCollection().ContainsKeyValue("time_seconds", "900")); // confirm that TRIP2 was taken not TRIP3.
+            Assert.AreEqual(coordinate2.Latitude, route.Segments[4].Latitude, E);
+            Assert.AreEqual(coordinate2.Longitude, route.Segments[4].Longitude, E);
+
+            // a trip 0 -> 3 on foot leaving 01/01/2015 01:45: should not take the transit link.
+            route = router.CalculateTransit(new DateTime(2015, 01, 01, 09, 45, 00), vehicle, vehicle, vehicle, resolved0, resolved3);
+            Assert.IsNotNull(route);
+            Assert.IsNotNull(route.Segments);
+            Assert.AreEqual(6, route.Segments.Length);
+            Assert.AreEqual(coordinate0.Latitude, route.Segments[0].Latitude, E);
+            Assert.AreEqual(coordinate0.Longitude, route.Segments[0].Longitude, E);
+            Assert.AreEqual(coordinate1.Latitude, route.Segments[1].Latitude, E);
+            Assert.AreEqual(coordinate1.Longitude, route.Segments[1].Longitude, E);
+            Assert.AreEqual(coordinate1.Latitude, route.Segments[2].Latitude, E);
+            Assert.AreEqual(coordinate1.Longitude, route.Segments[2].Longitude, E);
+            Assert.AreEqual(coordinate2.Latitude, route.Segments[3].Latitude, E);
+            Assert.AreEqual(coordinate2.Longitude, route.Segments[3].Longitude, E);
+            Assert.AreEqual(coordinate3.Latitude, route.Segments[4].Latitude, E);
+            Assert.AreEqual(coordinate3.Longitude, route.Segments[4].Longitude, E);
+            Assert.AreEqual(coordinate3.Latitude, route.Segments[5].Latitude, E);
+            Assert.AreEqual(coordinate3.Longitude, route.Segments[5].Longitude, E);
+        }
     }
 }

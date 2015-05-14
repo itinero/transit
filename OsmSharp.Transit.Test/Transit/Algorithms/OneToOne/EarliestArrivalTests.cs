5c160785fc87a8cd979765b1bbc7be37c9e53b52
xivk <ben@osmsharp.com>
2015-05-14 15:01:49 +0000

    Added a transfer test.

--- OsmSharp.Transit.Test/Transit/Algorithms/OneToOne/EarliestArrivalTests.cs
+++ OsmSharp.Transit.Test/Transit/Algorithms/OneToOne/EarliestArrivalTests.cs
@@ -86,7 +86,7 @@ namespace OsmSharp.Transit.Test.Transit.Algorithms.OneToOne
         }
 
         /// <summary>
-        /// Tests a successful two-hop with a one-connection db.
+        /// Tests a successful two-hop with a two-connection db.
         /// </summary>
         [Test]
         public void TestTwoHopsSuccessful()
@@ -119,6 +119,42 @@ namespace OsmSharp.Transit.Test.Transit.Algorithms.OneToOne
             Assert.IsTrue(algorithm.HasSucceeded);
             Assert.AreEqual(50 * 60, algorithm.Duration());
             Assert.AreEqual(new DateTime(2017, 05, 10, 08, 20, 00), algorithm.ArrivalTime());
+        }
+
+        /// <summary>
+        /// Tests a successful two-hop, one transfer with a two-connection db.
+        /// </summary>
+        [Test]
+        public void TestTwoHopsOneTransferSuccessful()
+        {
+            // build stub db.
+            var connectionsDb = new StubConnectionsDb();
+            connectionsDb.DepartureTimeConnections.Add(new Connection()
+            {
+                DepartureStop = 0,
+                DepartureTime = 0 * 60 + 3600 * 8, // departure at 08:00
+                ArrivalStop = 1,
+                ArrivalTime = 10 * 60 + 3600 * 8, // arrival at 08:10
+                TripId = 0
+            });
+            connectionsDb.DepartureTimeConnections.Add(new Connection()
+            {
+                DepartureStop = 1,
+                DepartureTime = 15 * 60 + 3600 * 8, // departure at 08:15
+                ArrivalStop = 2,
+                ArrivalTime = 25 * 60 + 3600 * 8, // arrival at 08:25
+                TripId = 1
+            });
+
+            // run algorithm.
+            var algorithm = new EarliestArrival(connectionsDb, 0, 2, new DateTime(2017, 05, 10, 07, 30, 00));
+            algorithm.Run();
+
+            // test results.
+            Assert.IsTrue(algorithm.HasRun);
+            Assert.IsTrue(algorithm.HasSucceeded);
+            Assert.AreEqual(55 * 60, algorithm.Duration());
+            Assert.AreEqual(new DateTime(2017, 05, 10, 08, 25, 00), algorithm.ArrivalTime());
         }
     }
 }

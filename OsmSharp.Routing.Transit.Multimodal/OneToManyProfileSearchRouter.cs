9c4bf75715337543f19c4bb84296d7038047e378
xivk <ben@osmsharp.com>
2015-07-26 18:32:01 +0000

    Added one-to-many profile search router.

--- OsmSharp.Routing.Transit.Multimodal/OneToManyProfileSearchRouter.cs
+++ OsmSharp.Routing.Transit.Multimodal/OneToManyProfileSearchRouter.cs
@@ -20,25 +20,27 @@ using OsmSharp.Math.Geo;
 using OsmSharp.Routing.Graph.Routing;
 using OsmSharp.Routing.Interpreter;
 using OsmSharp.Routing.Osm.Interpreter;
+using OsmSharp.Routing.Transit.Multimodal.Algorithms.OneToMany;
 using OsmSharp.Routing.Transit.Multimodal.Algorithms.OneToOne;
 using OsmSharp.Routing.Transit.Multimodal.Algorithms.Resolving;
 using OsmSharp.Routing.Transit.Multimodal.Data;
 using OsmSharp.Routing.Vehicles;
 using System;
+using System.Collections.Generic;
 
 namespace OsmSharp.Routing.Transit.Multimodal
 {
     /// <summary>
-    /// A profile router using a CSA and Dykstra to calculate multimodal earliest arrival routes.
+    /// A profile router using a CSA and Dykstra to calculate multimodal routes from one source location to many target locations.
     /// </summary>
-    public class ProfileSearchRouter : RouterBase
+    public class OneToManyProfileSearchRouter : RouterBase
     {
         private readonly GeoCoordinate _sourceLocation;
-        private readonly GeoCoordinate _targetLocation;
+        private readonly GeoCoordinate[] _targetLocations;
         private readonly int _maxSource;
         private readonly int _maxTarget;
         private readonly Vehicle _sourceVehicle;
-        private readonly Vehicle _targetVehicle;
+        private readonly Vehicle[] _targetVehicles;
         private readonly MultimodalConnectionsDb _db;
         private readonly IRoutingInterpreter _routingInterpreter;
         private readonly DateTime _departureTime;
@@ -47,16 +49,16 @@ namespace OsmSharp.Routing.Transit.Multimodal
         /// <summary>
         /// Creates a new earliest arrival router.
         /// </summary>
-        public ProfileSearchRouter(MultimodalConnectionsDb db, IRoutingInterpreter routingInterpreter, DateTime departureTime,
-            Vehicle sourceVehicle, GeoCoordinate source, Vehicle targetVehicle, GeoCoordinate target)
+        public OneToManyProfileSearchRouter(MultimodalConnectionsDb db, IRoutingInterpreter routingInterpreter, DateTime departureTime,
+            Vehicle sourceVehicle, GeoCoordinate source, Vehicle[] targetVehicles, GeoCoordinate[] targets)
         {
             _db = db;
             _routingInterpreter = routingInterpreter;
             _departureTime = departureTime;
             _sourceVehicle = sourceVehicle;
             _sourceLocation = source;
-            _targetVehicle = targetVehicle;
-            _targetLocation = target;
+            _targetVehicles = targetVehicles;
+            _targetLocations = targets;
             _lazyness = null;
             _maxSource = 10 * 60;
             _maxTarget = 10 * 60;
@@ -65,16 +67,16 @@ namespace OsmSharp.Routing.Transit.Multimodal
         /// <summary>
         /// Creates a new earliest arrival router.
         /// </summary>
-        public ProfileSearchRouter(MultimodalConnectionsDb db, IRoutingInterpreter routingInterpreter, DateTime departureTime,
-            Vehicle sourceVehicle, GeoCoordinate source, Vehicle targetVehicle, GeoCoordinate target, Func<float, float> lazyness)
+        public OneToManyProfileSearchRouter(MultimodalConnectionsDb db, IRoutingInterpreter routingInterpreter, DateTime departureTime,
+            Vehicle sourceVehicle, GeoCoordinate source, Vehicle[] targetVehicles, GeoCoordinate[] targets, Func<float, float> lazyness)
         {
             _db = db;
             _routingInterpreter = routingInterpreter;
             _departureTime = departureTime;
             _sourceVehicle = sourceVehicle;
             _sourceLocation = source;
-            _targetVehicle = targetVehicle;
-            _targetLocation = target;
+            _targetVehicles = targetVehicles;
+            _targetLocations = targets;
             _lazyness = lazyness;
             _maxSource = 10 * 60;
             _maxTarget = 10 * 60;
@@ -83,8 +85,8 @@ namespace OsmSharp.Routing.Transit.Multimodal
         /// <summary>
         /// Creates a new earliest arrival router.
         /// </summary>
-        public ProfileSearchRouter(MultimodalConnectionsDb db, IRoutingInterpreter routingInterpreter, DateTime departureTime,
-            Vehicle sourceVehicle, GeoCoordinate source, int maxSource, Vehicle targetVehicle, GeoCoordinate target, int maxTarget, 
+        public OneToManyProfileSearchRouter(MultimodalConnectionsDb db, IRoutingInterpreter routingInterpreter, DateTime departureTime,
+            Vehicle sourceVehicle, GeoCoordinate source, int maxSource, Vehicle[] targetVehicles, GeoCoordinate[] targets, int maxTarget,
             Func<float, float> lazyness)
         {
             _db = db;
@@ -92,45 +94,49 @@ namespace OsmSharp.Routing.Transit.Multimodal
             _departureTime = departureTime;
             _sourceVehicle = sourceVehicle;
             _sourceLocation = source;
-            _targetVehicle = targetVehicle;
-            _targetLocation = target;
+            _targetVehicles = targetVehicles;
+            _targetLocations = targets;
             _lazyness = lazyness;
             _maxSource = maxSource;
             _maxTarget = maxTarget;
         }
 
-        private ProfileSearch _algorithm;
+        private OneToManyProfileSearch _algorithm;
 
         /// <summary>
         /// Executes the actual run of the algorithm.
         /// </summary>
         protected override void DoRun()
         {
-            // resolve coordinates.
+            // instantiate source search.
             var sourceResolver = new PathSegmentVisitListResolver(_db.Graph, _sourceVehicle);
             var sourcePoint = sourceResolver.Resolve(_sourceLocation);
             var source = sourceResolver.GetHook(sourcePoint);
-            var targetResolver = new PathSegmentVisitListResolver(_db.Graph, _targetVehicle);
-            var targetPoint = targetResolver.Resolve(_targetLocation);
-            var target = sourceResolver.GetHook(targetPoint);
-
-            // instantiate source search.
             var sourceSearch = new OsmSharp.Routing.Transit.Multimodal.Algorithms.OneToMany.OneToManyDykstra(
                 _db.Graph, _routingInterpreter, _sourceVehicle, source, _maxSource, false);
-            // instantiate target search.
-            var targetSearch = new OsmSharp.Routing.Transit.Multimodal.Algorithms.OneToMany.OneToManyDykstra(
-                _db.Graph, _routingInterpreter, _targetVehicle, target, _maxTarget, true);
+
+            // instantiate target searches.
+            var targetSearches = new List<OsmSharp.Routing.Transit.Multimodal.Algorithms.OneToMany.OneToManyDykstra>(_targetLocations.Length);
+            for(var i = 0; i < targetSearches.Count; i++)
+            {
+                var targetResolver = new PathSegmentVisitListResolver(_db.Graph, _targetVehicles[i]);
+                var targetPoint = targetResolver.Resolve(_targetLocations[i]);
+                var target = sourceResolver.GetHook(targetPoint);
+                var targetSearch = new OsmSharp.Routing.Transit.Multimodal.Algorithms.OneToMany.OneToManyDykstra(
+                    _db.Graph, _routingInterpreter, _targetVehicles[i], target, _maxTarget, true);
+                targetSearches.Add(targetSearch);
+            }
 
             // instantiate earliest arrival search and run.
             if (_lazyness == null)
             {
-                _algorithm = new ProfileSearch(_db, _departureTime,
-                    sourceSearch, targetSearch);
+                _algorithm = new OneToManyProfileSearch(_db, _departureTime,
+                    sourceSearch, targetSearches.ToArray());
             }
             else
             {
-                _algorithm = new ProfileSearch(_db, _departureTime,
-                    sourceSearch, targetSearch, _lazyness);
+                _algorithm = new OneToManyProfileSearch(_db, _departureTime,
+                    sourceSearch, _lazyness, targetSearches.ToArray());
             }
 
             _algorithm.Run();
@@ -144,9 +150,9 @@ namespace OsmSharp.Routing.Transit.Multimodal
         /// Builds the route.
         /// </summary>
         /// <returns></returns>
-        public Route BuildRoute()
+        public Route BuildRoute(int i)
         {
-            var routeBuilder = new ProfileSearchRouteBuilder(_algorithm, _db);
+            var routeBuilder = new OneToManyProfileSearchRouteBuilder(_algorithm, _db, i);
             return routeBuilder.Build();
         }
     }

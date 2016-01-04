a67295e5e0eaf0db58b6081833dc8b5a74aff234
xivk <ben@osmsharp.com>
2016-01-04 17:07:07 +0100

    Start of automated functional test implementation by downloading files.

---
+++ OsmSharp.Routing.Transit.Test.Functional/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsmSharp.Routing.Transit.Test.Functional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // download test data.
+            var client = new WebClient();
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += client_DownloadFileCompleted;
+            client.DownloadFileAsync(new Uri("ftp://build.osmsharp.com/data/OSM/routing/develop/belgium.a.routing.zip"), 
+                "belgium.a.routing.zip");
+
+            Console.ReadLine();
+        }
+
+        static void client_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            File.Delete("belgium.a.routing");
+            ZipFile.ExtractToDirectory("belgium.a.routing.zip", ".");
+            Console.WriteLine("Belgium downloaded and extracted.");
+        }
+
+        static void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            Console.Write(".");
+        }
+    }
+}

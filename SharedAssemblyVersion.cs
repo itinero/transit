93095ff5db47fb890280076f8f72e7ec2c62259b
ben.abelshausen <ben@osmsharp.com>
2016-09-06 11:23:12 +0200

    Updated dependency.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyInformationalVersion("0.1.1-alpha")]
-[assembly: AssemblyVersion("0.1.1")]
+[assembly: AssemblyInformationalVersion("0.1.2-alpha")]
+[assembly: AssemblyVersion("0.1.2")]
